cf11e290cead4b8e7db4ce3a54c4271523f6bf26
Davey van Wijk <30650@ma-web.nl>
2020-11-02 12:19:19 +0100

    first commit

---
+++ verhaal1.py
@@ -0,0 +1,9 @@
+print("dit is een keuze verhaal over genaamd Armen uit Irak (Armen is christelijk)")
+keuze1 = input("gaat Armen vluchten of blijft hij Irak om z'n ouders te helpen? \n A. vluchten \n B. blijven")
+if keuze1 == "A":
+    keuze2 = input("waar gaat Armen naar toe vluchten? \n A. België \n B. Tukijë \n C. Nederland")
+	print(keuze)
+else:
+    keuze2 = input("de politie vind Armen en houd hem aan wat ga je nu doen \n A. er tegen vechten \n B. mee werken")
+    if keuze == "A":
+	print("Armen probeert tegen te vechten maar het lukt niet, Armen wordt dood geschoten")
