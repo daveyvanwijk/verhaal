3853f0358cdc49756fbf08efe565a869aa898faa
Davey van Wijk <30650@ma-web.nl>
2020-11-02 15:17:31 +0100

    second commit

--- verhaal.py
+++ verhaal.py
@@ -1,52 +1,63 @@
 print("dit is een keuze verhaal over een man genaamd Armen uit Irak (Armen is christelijk)")
-1 = input("gaat Armen vluchten of blijft hij Irak om z'n ouders te helpen? \n A. vluchten \n B. blijven")
-if 1 == "A":
-	2 = input("waar gaat Armen naar toe vluchten? \n A. België \n B. Tukijë \n C. Nederland")
-	if 2 == "A":
-		3 = input("hoe gaat Armen dat doen via boot is heel gevaarlijk en meeste mensen halen het niet maar lopen is ook heel gevaarlijk \n A. boor \n B. lopen")
-		if 3 == "A":
-			4 = input("Armen gaat een lange rijs aan hij moet eerst naar de kust toe en het geluk hebben dat er een boot staat. hij begint met lopen, hij komt iemand tegen die hem wilt helpen gaat Armen hem vertouwen of niet? \n ja \n nee")
-			if 4 == "ja":
-				5 = input("de man vraagt wat hij moet doen bij de kust ga je liegen of niet? \n A. liegen \n B. waarheid")
-                                if 5 == "A":
+"1" = input("gaat Armen vluchten of blijft hij Irak om z'n ouders te helpen? \n A. vluchten \n B. blijven")
+if "1" == "A":
+        "2" = input("waar gaat Armen naar toe vluchten? \n A. België \n B. Tukijë \n C. Nederland")
+        if "2" == "A":
+                "3" = input("hoe gaat Armen dat doen via boot is heel gevaarlijk en meeste mensen halen het niet maar lopen is ook heel gevaarlijk \n A. boor \n B. lopen")
+                if "3" == "A":
+                        "4" = input("Armen gaat een lange rijs aan hij moet eerst naar de kust toe en het geluk hebben dat er een boot staat. hij begint met lopen, hij komt iemand tegen die hem wilt helpen gaat Armen hem vertouwen of niet? \n ja \n nee")
+                        if "4" == "ja":
+                                "5" = input("de man vraagt wat hij moet doen bij de kust ga je liegen of niet? \n A. liegen \n B. waarheid")
+                                if "5" == "A":
                                         print("je zegt dat je daar werkt, de man geloofd het \n een paar uur verder kom je aan bij de kust maar er is niks de man rijd weer weg en je bent alleen niemand die weet waar hij is \n Armen sterft van de honger")
-	elif 2 == "B":
-		print("Armen kiest de makkelijkste mannier, hij gaat beginnen aan zijn rijs maar het is zwaarder dan hij denkt. hij doet z'n best maar haalt Turkije niet")
-		print("YOU DIED")
-	else:
-		101 = input("Armen bsluit om naar Nederland te gaan hij wilt via boot maar hij dat dat gevaarlijk gaat zijn maar lopen is net zo gevaarlijk, Armen begint te twijfelen om toch te blijven wat gaat hij doen? \n A. boot \n B. lopen \n C. toch blijven")                                                                                                        
-		if 101 == "A":
-			102 = input(" Armen begint aan een lange reid naar de kust, onderweg komt hij een man tegen de hem wilt helpen gaat Armen hem vertrouwen of niet \n ja \n nee")
-			if 103 == "ja":
-		elif 101 == "B":
-			print("Armen gaat het proberen om helemaal naar Nederland te lopen, nu is zijn doel om uit Irak te komen en hij ziet wel waar hij naar toe gaat. \n tijdens zijn lange reis komt hij politie tegen die hem aanhoud de politie agenten nemen hem mee naar het bureau. \n hij moet de gevangis in hij geeft het op hij zal nooit irak verlaten")
-				104 = input("Armen stapt in bij de man, de man vraagt wat hij bij de kust moet doen. gaat Armen liegen of de waarheid vertellen? \n A. waarheid \n B. liegen")
-				if 104 == "A":
-					105 = input("de man zegt daat hij een boot heeft en hij wilt Armen naar Nederland brengen, gaat Armen het aanbod aan of niet \n ja \n nee")
-					if 105 == "A":
-        					print("Armen gaat met hem mee en dit blijkt 1 van de beste keuzes te zijn van zijn leven want hij komt veilig aan in Nederland. Armen vraagt wat hij hem schuldig is maar de man geeft geen antwoord /n na een lange tijd krijgt Armen een Nederlands paspoort")
+
+        elif "2" == "B":
+                print("Armen kiest de makkelijkste mannier, hij gaat beginnen aan zijn rijs maar het is zwaarder dan hij denkt. hij doet z'n best maar haalt Turkije niet")
+                print("YOU DIED")
+        else:
+                "101" = input("Armen bsluit om naar Nederland te gaan hij wilt via boot maar hij dat dat gevaarlijk gaat zijn maar lopen is net zo gevaarlijk, Armen begint te twijfelen om toch te blijven wat gaat hij doen? \n A. boot \n B. lopen \n C. toch blijven")                                                                                                        
+                if "101" == "A":
+                        "102" = input(" Armen begint aan een lange reis naar de kust, onderweg komt hij een man tegen de hem wilt helpen gaat Armen hem vertrouwen of niet \n ja \n nee")
+                        if "102" == "ja":
+                                "301" == input("je stapt in de auto, de man vraagt wat hij doet liegen of waarheid \n A. liegen \n B. waarheid")
+                                if "301" == "A":
+                                        print("de man denkt er niks van en zet Armen af bij de kust er is niks en Armen zal nooit het land uit gaan")
+                                else:
+                                        "302" = input("de man zegt dat hij een boot heeft staan en wilt hem helpen, vertrouwen of afwijzen \n A. vertrouwen \n B. afwijzen")
+                                        if "302" == "A":
+                                                print("Armen besluit om de man te vertrouwen en Armen haalt Nederland")
+                                        else:
+                                                print("de man rijd weg en Armen is bij de kust niemand wilt hem helpen Armen zal nooit nederland berijken")
+                elif 101 == "B":
+                        print("Armen gaat het proberen om helemaal naar Nederland te lopen, nu is zijn doel om uit Irak te komen en hij ziet wel waar hij naar toe gaat. \n tijdens zijn lange reis komt hij politie tegen die hem aanhoud de politie agenten nemen hem mee naar het bureau. \n hij moet de gevangis in hij geeft het op hij zal nooit irak verlaten")
+                        "104" = input("Armen stapt in bij de man, de man vraagt wat hij bij de kust moet doen. gaat Armen liegen of de waarheid vertellen? \n A. waarheid \n B. liegen")
+                        if "104" == "A":
+                                "105" = input("de man zegt daat hij een boot heeft en hij wilt Armen naar Nederland brengen, gaat Armen het aanbod aan of niet \n ja \n nee")
+                                if "105" == "A":
+                                        print("Armen gaat met hem mee en dit blijkt 1 van de beste keuzes te zijn van zijn leven want hij komt veilig aan in Nederland. Armen vraagt wat hij hem schuldig is maar de man geeft geen antwoord /n na een lange tijd krijgt Armen een Nederlands paspoort")
                         else:
                                 print("de man denkt er niks van en hij zet hem af bij de kust")
                                 print("er is niks en hij kan het land niet uit")
-elif 1 == "B":
-	2 = input("de politie vind Armen en houd hem aan wat ga je nu doen \n A. er tegen vechten \n B. mee werken")
+elif "1" == "B":
+	"2" = input("de politie vind Armen en houd hem aan wat ga je nu doen \n A. er tegen vechten \n B. mee werken")
 	if keuze == "A":
- 		print("Armen probeert tegen te vechten maar het lukt niet, Armen wordt dood geschoten")
+                print("Armen probeert tegen te vechten maar het lukt niet, Armen wordt dood geschoten")
         else:
-               201 = input("de politie vind dat Armen een goed persoon is, brijft Armen toch of gaat hij toch vluchten \n A. vluchten \n B. blijven")
-               if 201 == "A":
-                       202 = input("Armen gaat toch vluchten hij denkt dat hij geluk had en dat dat geen tweede keer gaat gebeuren, waar gaat Armen naar toe \n A. Nederland \n B. Turkije")
-                       if 202 == "A":
-                               203 = input("hoe gaat Armen naar Nederland? via lopen of met de auto. \n A. lopen \n B. auto")
-                               if 203 == "A":
-                                       print("Armen besluit om te lopen omdat hij bang is om gepakt te worden, Armen heeft honger maar hij heeft niks bij zich. Armen sterft aan de honger")
-                else:
-                        print("Armen besluit om te blijven, maar hij wordt gepakt en hij moet dit keer 2 jaar in de gevangenis te zitten")
-                        else:
-                                204 = input("gaat Armen lopen of met de auto? \n A. auto \n B. lopen")
+                "201" = input("de politie vind dat Armen een goed persoon is, brijft Armen toch of gaat hij toch vluchten \n A. vluchten \n B. blijven")
+                if "201" == "A":
+                       "202" = input("Armen gaat toch vluchten hij denkt dat hij geluk had en dat dat geen tweede keer gaat gebeuren, waar gaat Armen naar toe \n A. Nederland \n B. Turkije")
+                       if "202" == "A":
+                               "203" = input("hoe gaat Armen naar Nederland? via lopen of met de auto. \n A. lopen \n B. auto")
+                       else:
+                                "204" = input("gaat Armen lopen of met de auto? \n A. auto \n B. lopen")
                                 if 204 == "A":
                                         print("Armen gaat rijden naar Turkije het gaat flekkenloos en Armen heeft het gehaald")
                                         
-                
+
+                               if "203" == "A":
+                                       print("Armen besluit om te lopen omdat hij bang is om gepakt te worden, Armen heeft honger maar hij heeft niks bij zich. Armen sterft aan de honger")
+                else:
+                        print("Armen besluit om te blijven, maar hij wordt gepakt en hij moet dit keer 2 jaar in de gevangenis te zitten")
+                                       
                              
                 
